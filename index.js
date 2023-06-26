11a6e7d47e604a9608cf0fc53556976a207401b4
Roland Hummel <roland.hummel@rio.cloud>
2023-06-26 09:25:03 +0200

    Introduce logo

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-import { argv } from "node:process";
+import { argv } from 'node:process';
+import { rioLogo } from './src/rioLogo.js';
 
-console.info(
-  "Hello, world! The first two parameters you gave me are:",
-  argv[2],
-  argv[3]
-);
+rioLogo();
+
+console.info('Hello, world! The first two parameters you gave me are:', argv[2], argv[3]);
