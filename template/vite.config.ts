94a0a169bc2e17528530d3c3d9e2ce32eb65021f
Roland Hummel <roland.hummel@rio.cloud>
2023-08-04 17:21:30 +0200

    (refactor) Switch to vitest's defineConfig for typing

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -1,6 +1,4 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 
