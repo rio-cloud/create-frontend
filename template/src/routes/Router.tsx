fbdc3e9423a02f57a714a506a3f436821832583c
Roland Hummel <roland.hummel@rio.cloud>
2023-10-06 14:12:54 +0200

    feature: add "suspendWithPageLoad" feature to simplify lazy-loading

--- template/src/routes/Router.tsx
+++ template/src/routes/Router.tsx
@@ -1,14 +1,13 @@
-import { lazy } from 'react';
 import { createHashRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 import { ErrorBoundary } from '../components/ErrorBoundary';
-import SuspendedWithSpinner from '../components/SuspendedWithSpinner';
 import AppLayout from '../layout/AppLayout';
+import { suspendPageLoad } from './suspendPageLoad';
 
 // Lazy load pages for better performance and automatically split the bundle accordingly
-const Intro = lazy(() => import('../pages/Intro'));
-const More = lazy(() => import('../pages/More'));
-const UserSidebarLoader = lazy(() => import('../features/users/userSidebar/UserSidebarLoader'));
+const Intro = suspendPageLoad(() => import('../pages/Intro'));
+const More = suspendPageLoad(() => import('../pages/More'));
+const UserSidebarLoader = suspendPageLoad(() => import('../features/users/userSidebar/UserSidebarLoader'));
 
 export const DEFAULT_ROUTE = '/intro';
 export const ROUTE_MORE = '/more';
@@ -18,32 +17,9 @@ export const routes = [DEFAULT_ROUTE, ROUTE_MORE];
 export const router = createHashRouter(
     createRoutesFromElements(
         <Route path="/" element={<AppLayout />}>
-            <Route
-                path={DEFAULT_ROUTE}
-                errorElement={<ErrorBoundary />}
-                element={
-                    <SuspendedWithSpinner>
-                        <Intro />
-                    </SuspendedWithSpinner>
-                }
-            />
-            <Route
-                path={ROUTE_MORE}
-                errorElement={<ErrorBoundary />}
-                element={
-                    <SuspendedWithSpinner>
-                        <More />
-                    </SuspendedWithSpinner>
-                }
-            >
-                <Route
-                    path=":userId"
-                    element={
-                        <SuspendedWithSpinner>
-                            <UserSidebarLoader />
-                        </SuspendedWithSpinner>
-                    }
-                />
+            <Route path={DEFAULT_ROUTE} errorElement={<ErrorBoundary />} element={<Intro />} />
+            <Route path={ROUTE_MORE} errorElement={<ErrorBoundary />} element={<More />}>
+                <Route path=":userId" element={<UserSidebarLoader />} />
             </Route>
         </Route>
     )
---
+++ template/src/routes/suspendPageLoad.tsx
@@ -0,0 +1,17 @@
+import SuspendedWithSpinner from '../components/SuspendedWithSpinner';
+import { type ComponentType, lazy, type PropsWithoutRef } from 'react';
+
+type DynamicImportFactory<T> = () => Promise<{ default: ComponentType<T> }>;
+
+/**
+ * Lazy-loads the given dynamic import and renders the imported component in a suspense with a big spinner as fallback.
+ */
+export const suspendPageLoad = <PropsType extends {}>(factory: DynamicImportFactory<PropsType>) => {
+    const LazyComponent = lazy(factory);
+    // eslint-disable-next-line react/display-name
+    return (props: PropsWithoutRef<PropsType>) => (
+        <SuspendedWithSpinner>
+            <LazyComponent {...props} />
+        </SuspendedWithSpinner>
+    );
+};
