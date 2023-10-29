ba6d53ec1f2a61d652cd46783ecdf06243abb6e9
Roland Hummel <roland.hummel@rio.cloud>
2023-10-29 17:13:53 +0100

    (refactor) remove import normalization
    not needed anymore thanks to ESM-goodness

--- template/src/configuration/lang/services.ts
+++ template/src/configuration/lang/services.ts
@@ -1,18 +1,11 @@
 import { Store } from '@reduxjs/toolkit';
 import { reportErrorToSentry } from '../setup/sentry';
-import getOr from 'lodash/fp/getOr';
-
 import { displayMessagesFetched, localeChanged } from './langSlice';
 import { DEFAULT_LOCALE, getSupportedLocale as defaultGetSupportedLocale } from './lang';
 import { trace } from '../setup/trace';
 
-const normalizeDynamicImport = (imported: unknown) => getOr(imported, 'default', imported);
-
-const importDisplayMessages = (locale: string) =>
-    import(`../../features/translations/${locale}.json`).then(normalizeDynamicImport).catch((error: unknown) => {
-        reportErrorToSentry(error);
-        return error;
-    });
+const importDisplayMessages = async (locale: string) =>
+    (await import(`../../features/translations/${locale}.json`)).default;
 
 export const configureFetchDisplayMessages =
     (store: Store, fetchDisplayMessages = importDisplayMessages, getSupportedLocale = defaultGetSupportedLocale) =>
