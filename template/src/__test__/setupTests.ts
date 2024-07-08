06b2fd295c29660fa8e76fb0f753dfa141724c73
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2024-07-08 21:41:56 +0200

    (fix) add a workaround and docu for Vitest fake timers

--- template/src/__test__/setupTests.ts
+++ template/src/__test__/setupTests.ts
@@ -21,3 +21,11 @@ vi.stubGlobal('IntersectionObserver', IntersectionObserverMock);
 // adds the 'fetchMock' global variable and rewires 'fetch' global to call 'fetchMock'
 const fetchMock = createFetchMock(vi);
 fetchMock.enableMocks();
+
+// RTL internally only supports `jest.advanceTimersByTime`. See:
+// https://github.com/testing-library/react-testing-library/blob/main/src/pure.js#L51
+// Therefore, we need to mock it with the Vitest version. Otherwise, tests
+// using fake timers might break by e.g. timing out.
+vi.stubGlobal('jest', {
+    advanceTimersByTime: (time: number) => vi.advanceTimersByTime(time),
+});
