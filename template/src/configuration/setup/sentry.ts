141e48642ba4d0a753488bf787349016e9a59f96
Roland Hummel <roland.hummel@rio.cloud>
2024-07-26 11:19:27 +0200

    (feature) use APP_NAME from package.json, as well

--- template/src/configuration/setup/sentry.ts
+++ template/src/configuration/setup/sentry.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/browser';
 import { config } from '../../config';
 
 if (import.meta.env.PROD) {
-    const release = config.serviceVersion;
+    const release = APP_VERSION;
     const environment = config.serviceEnvironment;
     const dsn = config.sentryToken;
 
