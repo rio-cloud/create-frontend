d42c4cc4eeabbed09662a8a0a58207ee2e2e58f1
Roland Hummel <roland.hummel@rio.cloud>
2024-10-09 14:04:49 +0200

    (refactor) simplify testRender

--- template/src/__test__/testRender.tsx
+++ template/src/__test__/testRender.tsx
@@ -3,43 +3,43 @@ import { render, type RenderOptions } from '@testing-library/react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 import { Provider } from 'react-redux';
-import type { ReactElement, ReactNode } from 'react';
+import type { PropsWithChildren, ReactElement } from 'react';
 import type { Store } from '@reduxjs/toolkit';
 
-import { store as defaultStore } from '../configuration/setup/store';
+import { type RootState, store as defaultStore } from '../configuration/setup/store';
 import messages from '../features/translations/en-GB.json';
+import { DEFAULT_LOCALE } from '../configuration/lang/lang';
 
-const getTestRouter = (children: ReactNode) => {
+type TestWrapperProps = {
+    store?: Store<RootState>;
+};
+
+const TestWrapper = ({ store, children }: PropsWithChildren<TestWrapperProps>) => {
     const routes = [{ path: '/', element: children }];
+    const router = createMemoryRouter(routes, { initialEntries: ['/'], initialIndex: 1 });
 
-    return createMemoryRouter(routes, {
-        initialEntries: ['/'],
-        initialIndex: 1,
-    });
+    const content = (
+        <IntlProvider locale={DEFAULT_LOCALE} messages={messages}>
+            <RouterProvider router={router} />
+        </IntlProvider>
+    );
+    return store ? <Provider store={store}>{content}</Provider> : content;
 };
 
-const createWrapper = (children: ReactNode, store?: Store) => {
-    const WrapperComponent = ({ children }: { children: ReactNode }) => {
-        const content = (
-            <IntlProvider locale='en-GB' messages={messages}>
-                <RouterProvider router={getTestRouter(children)} />
-            </IntlProvider>
-        );
-        return store ? <Provider store={store}>{content}</Provider> : content;
-    };
-    return WrapperComponent;
-};
-
-export const renderWithRouter = (element: ReactElement, renderOptions?: RenderOptions) => {
+export const renderTest = (element: ReactElement, renderOptions?: RenderOptions) => {
     const user = userEvent.setup();
-    const defaultOptions = { wrapper: createWrapper(element) };
+    const defaultOptions: RenderOptions = { wrapper: () => <TestWrapper>{element}</TestWrapper> };
 
     return { ...render(element, { ...defaultOptions, ...renderOptions }), user };
 };
 
-export const renderWithStore = (element: ReactElement, store: Store = defaultStore, renderOptions?: RenderOptions) => {
+export const renderTestWithStore = (
+    element: ReactElement,
+    store: Store = defaultStore,
+    renderOptions?: RenderOptions
+) => {
     const user = userEvent.setup();
-    const defaultOptions = { wrapper: createWrapper(element, store) };
+    const defaultOptions: RenderOptions = { wrapper: () => <TestWrapper store={store}>{element}</TestWrapper> };
 
-    return { ...render(element, { ...defaultOptions, ...renderOptions }), user };
+    return { ...render(element, { ...defaultOptions, ...renderOptions }), user, store };
 };
