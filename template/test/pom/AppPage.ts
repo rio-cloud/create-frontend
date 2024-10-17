f034ff773b25c042a64b044050b721557436a68b
Roland Hummel <roland.hummel@rio.cloud>
2024-10-17 12:05:00 +0200

    (feature) re-structure Playwright setup a bit
    - one central testSetup file that provides the extended test function with all POMs as fixtures
    - dedicated directories for POMs and specs
    - no more hard-coded BASE_URL (it's set by Playwright, anyway)
    - tiny refactoring: move "Release notes" locator into POM
    - no more need to explicitly define & export the POM type - it's only used in the testSetup file, and can be inferred there, anyway

--- template/test/pom/AppPage.ts
+++ template/test/pom/AppPage.ts
@@ -1,6 +1,4 @@
-import { type Page, test as base } from '@playwright/test';
-
-export const BASE_URL = 'http://localhost:3000/#/';
+import type { Page } from '@playwright/test';
 
 /**
  * Creates a "page object model" (POM).
@@ -14,28 +12,20 @@ export const appPage = (page: Page) => {
     const body = page.locator('.ApplicationLayoutBody');
     const locationMenu = page.locator('.ModuleNavigation-dropdown');
     const actionBarItems = page.locator('.ActionBarItemIcon');
+    const releaseNotes = page.getByText('Release notes');
 
     const open = async (queryParams: Record<string, string> = {}) => {
         const search = new URLSearchParams(queryParams).toString();
-        await page.goto(`${BASE_URL}${search ? `?${search}` : ''}`);
+        if (search) {
+            await page.goto(`/?${search}`);
+        } else {
+            await page.goto('/');
+        }
     };
 
     const openServiceInfo = async () => {
         await actionBarItems.first().click();
     };
 
-    return { page, body, locationMenu, actionBarItems, open, openServiceInfo };
+    return { page, body, locationMenu, actionBarItems, releaseNotes, open, openServiceInfo };
 };
-
-export type AppPage = ReturnType<typeof appPage>;
-
-/**
- * Extends the default Playwright `test` function with a "fixture".
- *
- * This allows specs to directly use the POM instead of having to create it every single time.
- *
- * @see https://playwright.dev/docs/test-fixtures
- */
-export const test = base.extend<{ appPage: AppPage }>({
-    appPage: async ({ page }, use) => await use(appPage(page)),
-});
