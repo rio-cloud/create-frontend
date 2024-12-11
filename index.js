3a0be7e30ab09a41883b0246b6e5fd3eef4659f4
Roland Hummel <roland.hummel@rio.cloud>
2024-12-11 13:56:08 +0100

    (chore) update deps

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url';
 import { dirname, relative, resolve } from 'node:path';
 import boxen from 'boxen';
 import chalk from 'chalk';
-import gradient from 'gradient-string';
+import { retro } from 'gradient-string';
 import { cli } from './src/cli.js';
 import { getTasks } from './src/tasks.js';
 
@@ -34,7 +34,7 @@ try {
         console.log(`> ${chalk.cyan(`cd ${relative(cwd(), config.outputDir)}`)}`);
         console.log(`> ${chalk.cyan('npm install')}`);
         console.log(`> ${chalk.cyan('npm start')}`);
-        console.log(boxen(chalk.bold(gradient.retro('Have a nice day :)')), { margin: 1, padding: 1 }));
+        console.log(boxen(chalk.bold(retro('Have a nice day :)')), { margin: 1, padding: 1 }));
     }
 } catch (e) {
     console.error(chalk.bgRed.bold('Something went wrong. Please check the output above.'));
