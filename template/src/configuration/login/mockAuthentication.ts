bed7f0e546339c620a13ed5377ce4c9860c2f221
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2025-06-19 22:24:58 +0200

    refactor: move the login and user session related code into the login folder
    The code in the oauth.ts file was highly related to the code in the
    login folder. It made no sense to have it in the setup folder.
    
    Furthermore, the configuration/index.ts file also contained highly login
    specific code like the creation and configuration of the user manager.
    
    This commit also simplifies the mocked authentication. There is no real
    need to implement a stripped down version of the user manager, because
    the only thing the silentSignin method did was a call to the onSessionRenewed
    hook with static data.

---
+++ template/src/configuration/login/mockAuthentication.ts
@@ -0,0 +1,26 @@
+import { SessionRenewedResult } from '.';
+import { config } from '../../config';
+
+// enables mocking of authentication in non-production
+export const shouldMockAuthentication = () => import.meta.env.MODE !== 'production' && config.login.mockAuthorization;
+
+export const mockSession: SessionRenewedResult = {
+    accessToken: 'valid-mocked-oauth-bogus-token',
+    idToken: 'id_token',
+    locale: config.login.mockLocale ?? 'en-GB',
+    profile: {
+        sub: 'prod-rio-users:mock-user',
+        azp: 'test-client',
+        account: 'mockaccount',
+        tenant: config.login.mockTenant,
+        givenName: 'Test',
+        familyName: 'Client',
+        name: 'Test Client',
+        locale: config.login.mockLocale,
+        email: 'test@example.com',
+        iss: 'Issuer Identifier',
+        aud: 'Audience(s): client_id',
+        exp: 10,
+        iat: 5,
+    },
+};
